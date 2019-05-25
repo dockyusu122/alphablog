87d5d6872a132f0da7ebc8ff0af8815607549846
Yusuke Togawa <yusuk.togawa1@gmail.com>
2019-05-25 07:23:33 +0000

    yea

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'welcome#home'
-  get 'welcome/about', to: 'welcome#about'
+  root 'pages#home'
+  get  'pages/about', to: 'pages#about'
+  
+  resources :articles
 end
